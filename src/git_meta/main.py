f294ba13d0b3011a3ac5d55cf4ace097d0068581
Bill Wallis <bill@tasman.ai>
2026-02-25 07:47:50 +0000

    feat: add `update` subparser

--- src/git_meta/main.py
+++ src/git_meta/main.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import argparse
 import pathlib
 import textwrap
+import tomllib
 from collections.abc import Sequence
 
 import git
@@ -10,6 +11,7 @@ import git
 SUCCESS = 0
 FAILURE = 1
 HERE = pathlib.Path(__file__).parent
+PYPROJECT = HERE.parent.parent / "pyproject.toml"
 
 RED = "\033[1;31m"
 GREEN = "\033[1;32m"
@@ -26,6 +28,11 @@ def colour(text: str, colour_: str) -> str:
     """
 
     return f"{colour_}{text}{RESET}"
+
+
+def _get_version() -> str:
+    pyproject = tomllib.loads(PYPROJECT.read_text(encoding="utf-8"))
+    return pyproject["project"]["version"]
 
 
 def _get_git_repos(directory: pathlib.Path) -> list[git.Repo]:
@@ -52,6 +59,31 @@ def _get_status_colour(repository: git.Repo) -> str:
         col = RED
 
     return col
+
+
+def _pull_repo_main_branch(repository: git.Repo) -> None:
+    """
+    Pull the default branch.
+    """
+
+    status = repository.git.status()
+    if "Your branch is behind" in status and (
+        "On branch main" in status or "On branch master" in status
+    ):
+        try:
+            # print(colour(f"\n{repository.working_tree_dir}", BOLD + BLUE))
+            print(repository.git.pull())
+        except TypeError:
+            print(colour("pull skipped", GREY))
+
+
+def pull_repo_main_branches(root_directory: pathlib.Path) -> None:
+    """
+    Pull the default branches.
+    """
+
+    for repo in _get_git_repos(root_directory):
+        _pull_repo_main_branch(repo)
 
 
 def print_repo_statuses(repositories: list[git.Repo], print_all: bool) -> None:
@@ -110,21 +142,49 @@ def git_report(
     print_repo_statuses(repos, print_all)
 
 
+def _update(args: argparse.Namespace) -> int:
+    pull_repo_main_branches(pathlib.Path(getattr(args, "root-dir")))
+    return SUCCESS
+
+
+def _report(args: argparse.Namespace) -> int:
+    git_report(
+        root_directory=pathlib.Path(getattr(args, "root-dir")),
+        print_all=args.print_all,
+    )
+    return SUCCESS
+
+
 def main(argv: Sequence[str] | None = None) -> int:
     """
     Parse the arguments and run the command.
     """
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("root-dir")
-    parser.add_argument("--print-all", action=argparse.BooleanOptionalAction)
-    args = parser.parse_args(argv)
+    parser.add_argument(
+        "-v",
+        "--version",
+        action="version",
+        version=_get_version(),
+    )
+    subparsers = parser.add_subparsers(dest="command")
 
-    git_report(
-        root_directory=pathlib.Path(getattr(args, "root-dir")),
-        print_all=args.print_all,
+    parser__update = subparsers.add_parser("update")
+    parser__update.add_argument("root-dir")
+
+    parser__report = subparsers.add_parser("report")
+    parser__report.add_argument("root-dir")
+    parser__report.add_argument(
+        "--print-all", action=argparse.BooleanOptionalAction
     )
 
+    args = parser.parse_args(argv)
+    if args.command == "update":
+        return _update(args)
+    if args.command == "report":
+        return _report(args)
+
+    parser.print_help()
     return SUCCESS
 
 
