ae7bf5ca67ad32dfb67cd2ebf8cabcb8da633170
Bill <bill@thewallisfamily.co.uk>
2026-03-01 10:01:23 +0000

    feat: adjust print colours

--- src/git_meta/main.py
+++ src/git_meta/main.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import enum
 import pathlib
 import textwrap
 
@@ -9,9 +10,20 @@ RED = "\033[1;31m"
 GREEN = "\033[1;32m"
 YELLOW = "\033[1;33m"
 BLUE = "\033[1;34m"
+MAGENTA = "\033[1;35m"
+CYAN = "\033[1;36m"
 GREY = "\033[38;5;240m"
 BOLD = "\033[1m"
 RESET = "\033[0m"
+
+
+# TODO: These are not mutually exclusive, so an Enum is not appropriate
+class RepoStatus(enum.StrEnum):
+    CLEAN_AND_UPDATED = enum.auto()
+    UNTRACKED_FILES = enum.auto()
+    BEHIND_REMOTE = enum.auto()
+    DIRTY = enum.auto()
+    UNKNOWN = enum.auto()
 
 
 def colour(text: str, colour_: str) -> str:
@@ -37,15 +49,28 @@ def _get_git_repos(directory: pathlib.Path) -> list[git.Repo]:
     )
 
 
-def _get_status_colour(repository: git.Repo) -> str:
+def _get_repo_status(repository: git.Repo) -> RepoStatus:
     status = repository.git.status()
-    col = GREEN
-    if "Your branch is behind" in status or "Untracked files" in status:
-        col = YELLOW
+    if "nothing to commit, working tree clean" in status:
+        return RepoStatus.CLEAN_AND_UPDATED
     if repository.is_dirty():
-        col = RED
+        return RepoStatus.DIRTY
+    if "Your branch is behind" in status:
+        return RepoStatus.BEHIND_REMOTE
+    if "Untracked files" in status:
+        return RepoStatus.UNTRACKED_FILES
 
-    return col
+    return RepoStatus.UNKNOWN
+
+
+def _get_status_colour(repository_status: RepoStatus) -> str:
+    return {
+        RepoStatus.CLEAN_AND_UPDATED: GREEN,
+        RepoStatus.DIRTY: RED,
+        RepoStatus.BEHIND_REMOTE: YELLOW,
+        RepoStatus.UNTRACKED_FILES: YELLOW,
+        RepoStatus.UNKNOWN: MAGENTA,
+    }[repository_status]
 
 
 def _pull_repo_main_branch(repository: git.Repo) -> None:
@@ -96,14 +121,17 @@ def print_repo_statuses(repositories: list[git.Repo], print_all: bool) -> None:
                 print(colour(f"    remote URL: {remote_url}", col))
             print(
                 textwrap.indent(
-                    colour(repo.git.status(), _get_status_colour(repo)),
+                    colour(
+                        repo.git.status(),
+                        _get_status_colour(_get_repo_status(repo)),
+                    ),
                     prefix=" " * 4,
                 )
             )
 
     print("\nRepositories that need reviewing:")
     for repo in repositories:
-        col = _get_status_colour(repo)
+        col = _get_status_colour(_get_repo_status(repo))
 
         if all(b.name in ["main", "master"] for b in repo.branches):
             if repo.is_dirty():
