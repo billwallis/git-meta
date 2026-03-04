23298e5f9a6f968e2e2a44ed5c6fb7d5e0492a1e
Bill Wallis <bill@tasman.ai>
2026-03-04 11:42:09 +0000

    test: add test for environment variable setting

--- tests/test__constants.py
+++ tests/test__constants.py
@@ -6,7 +6,7 @@ import pytest
 from git_meta import constants
 
 
-def test__home_path_defaults_are_used(monkeypatch: pytest.MonkeyPatch):
+def test__constants__default_values_are_used(monkeypatch: pytest.MonkeyPatch):
     env_vars = [
         "HOME",
         "USERPROFILE",
@@ -28,3 +28,29 @@ def test__home_path_defaults_are_used(monkeypatch: pytest.MonkeyPatch):
     assert constants.STATE_HOME == here / ".local/state"
     assert constants.CACHE_HOME == here / ".cache"
     assert constants.CONFIG_HOME == here / ".config"
+
+
+def test__constants__environment_variables_are_used(
+    monkeypatch: pytest.MonkeyPatch,
+    tmp_path: pathlib.Path,
+):
+    env_vars = {
+        "HOME": tmp_path,
+        "USERPROFILE": tmp_path,
+        "XDG_DATA_HOME": tmp_path / "data",
+        "XDG_STATE_HOME": tmp_path / "state",
+        "XDG_CACHE_HOME": tmp_path / "cache",
+        "XDG_CONFIG_HOME": tmp_path / "config",
+    }
+    for env_var_name, env_var_value in env_vars.items():
+        monkeypatch.setenv(name=env_var_name, value=str(env_var_value))
+
+    # We need to reload the module since we need the env vars to have been
+    # updated _before_ defining the constants
+    importlib.reload(constants)
+
+    assert constants.HOME == tmp_path
+    assert constants.DATA_HOME == tmp_path / "data"
+    assert constants.STATE_HOME == tmp_path / "state"
+    assert constants.CACHE_HOME == tmp_path / "cache"
+    assert constants.CONFIG_HOME == tmp_path / "config"
